df60f8805cdd0f2f83908a7ae65b16c353cde441
sasakura_870 <a_20100409@yahoo.co.jp>
2020-01-29 14:12:44 +0900

    update wiki_controllerのmoduleを全て削除
    ディレクトリ階層の見直しによる修正

--- app/controllers/wiki_controller.rb
+++ app/controllers/wiki_controller.rb
@@ -1,13 +1,9 @@
-module API
-  module V1
-    class WikiController < ApplicationController
-      def index
-        render json: { message: 'index_test' }
-      end
+class WikiController < ApplicationController
+  def index
+    render json: { message: 'index_test' }
+  end
 
-      def show
-        render json: { message: 'test' }
-      end
-    end
+  def show
+    render json: { message: 'test' }
   end
 end

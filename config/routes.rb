8a799a6ce7bc6f8829bc311acd2a855d09305fda
sasakura_870 <a_20100409@yahoo.co.jp>
2020-01-29 13:07:35 +0900

    update routeの設定
    コントローラのフォルダ分け済み

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  get 'wiki/show'
+  namespace :api do
+    namespace :v1 do
+      resources :wiki, only: %i[show]
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

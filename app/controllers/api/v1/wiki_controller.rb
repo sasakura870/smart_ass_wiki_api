7b0fe2ca9814fef00cca317c08652cfac1da72ef
sasakura_870 <a_20100409@yahoo.co.jp>
2020-01-29 13:15:59 +0900

    update APIテスト用の処理の記述

--- app/controllers/api/v1/wiki_controller.rb
+++ app/controllers/api/v1/wiki_controller.rb
@@ -1,4 +1,9 @@
 class WikiController < ApplicationController
+  def index
+    render json: { message: 'index_test' }
+  end
+
   def show
+    render json: { message: 'test' }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :wiki, only: %i[show]
+      resources :wiki, only: %i[index show]
     end
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'api/v1/wiki'
 end
